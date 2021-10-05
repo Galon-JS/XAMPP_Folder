469ecfec505efb118197e20678d2b4bb188148da
Dragan <milosevdragan1988@gmail.com>
2021-10-05 21:52:06 +0200

    r
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- _03_JavaScript_Application_Development/09_Kreiranje_React_aplikacije/my-first-react-app/src/index.js
+++ _03_JavaScript_Application_Development/09_Kreiranje_React_aplikacije/my-first-react-app/src/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  React.createElement('div', null, 'Hello world!'),
-  document.getElementById('root')
-);
+const colors = ['Amaranth', 'Auburn', 'Chestnut', 'Frostbite']
+const elem = <ul>
+  {
+    colors.map((value, index) => {
+      return <li key={index}>{value}</li>
+    })
+  }
+</ul>
+ReactDOM.render(elem, document.getElementById('root'))
+
+// const user = {
+//   firstName: 'Ana',
+//   lastName: 'Todoric',
+//   fullName: function () {
+//     return this.firstName + ' ' + this.lastName
+//   }
+// }
+// const elem = <h1 className="heading" id="my-heading">{user.fullName()}</h1>
+// ReactDOM.render(elem, document.getElementById('root'))
+
+// const content = "I am React Element"
+// const elem = <h1 className="heading" id="my-heading">{content}</h1>
+// ReactDOM.render(elem, document.getElementById('root'))
+
+// const elem = <h1 className="heading" id="my-heading">I am React Element</h1>
+// ReactDOM.render(elem, document.getElementById('root'))
+
+// ReactDOM.render(
+  // React.createElement('div', null, 'Hello world!'),
+  // document.getElementById('root')
+// );
+
+// let elem = React.createElement(
+  // 'h1',
+  // {
+    // className: 'heading',
+    // id: 'my-heading',
+  // },
+  // 'I am React Element'
+// )
+// ReactDOM.render(elem, document.getElementById('root'))
