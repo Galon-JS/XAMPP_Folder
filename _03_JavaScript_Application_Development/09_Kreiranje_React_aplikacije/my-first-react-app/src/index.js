6e0c9f6e13758ce2dfde5e7997c365a5de70c20f
Dragan <milosevdragan1988@gmail.com>
2021-10-06 10:15:27 +0200

    g
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- _03_JavaScript_Application_Development/09_Kreiranje_React_aplikacije/my-first-react-app/src/index.js
+++ _03_JavaScript_Application_Development/09_Kreiranje_React_aplikacije/my-first-react-app/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// let age = 18
+// const elem = (age > 17) ? <div>You are welcome !</div> : <div>You can't enter !</div>
+// ReactDOM.render(elem, document.getElementById('root'))
+
+// function makeMessage(age) {
+// 	if (age > 17) {
+// 		return <div>You are welcome !</div>
+// 	} else {
+// 		return <div>You can't enter !</div>
+// 	}
+// }
+
+// ReactDOM.render(makeMessage(18), document.getElementById('root'))
+
+// const elem = <ul>
+//   <li>Amaranth</li>
+//   <li>Auburn</li>
+//   <li>Chestnut</li>
+//   <li>Frostbite</li>
+// </ul>
+// ReactDOM.render(elem, document.getElementById('root'))
+
 const colors = ['Amaranth', 'Auburn', 'Chestnut', 'Frostbite']
 const elem = <ul>
   {
