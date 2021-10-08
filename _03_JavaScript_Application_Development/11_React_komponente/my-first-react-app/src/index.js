b3d47cdaf8aaf80528362ab4b9a53ef974897067
Dragan <milosevdragan1988@gmail.com>
2021-10-08 20:07:32 +0200

    x
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- _03_JavaScript_Application_Development/11_React_komponente/my-first-react-app/src/index.js
+++ _03_JavaScript_Application_Development/11_React_komponente/my-first-react-app/src/index.js
@@ -1,7 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function SayHelloWorld() {
-    return <h1> Hello World !!!</h1>;
+class SayHelloWorld extends React.Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.state = { code: Math.random().toString(36).substring(7) }
+        
+        this.generateCode = this.generateCode.bind(this)
+    }
+
+    generateCode() {
+        this.setState({ code: Math.random().toString(36).substring(7)})        
+    }
+
+    render2() {
+        return (<div>
+            <h1>Your code is: {this.state.code}</h1>
+            <button onClick={this.generateCode}>Generate New Code</button>
+        </div>)
+    }
+
+    render() {
+        return <h1>Hello {this.state.code}</h1> 
+    }
 }
-ReactDOM.render(SayHelloWorld(), document.getElementById('root'))
+ReactDOM.render(<SayHelloWorld name="Ana" />, document.getElementById('root'))
+
+// class SayHelloWorld extends React.Component {
+//     render() {
+//         return <h1> Hello World REACT!!!</h1> 
+//     }
+// }
+// ReactDOM.render(<SayHelloWorld />, document.getElementById('root'))
+
+// function SayHelloWorld() {
+//     return <h1> Hello World REACT!!!</h1>;
+// }
+// ReactDOM.render(SayHelloWorld(), document.getElementById('root'))
+
+// const elem = <SayHelloWorld />
+// ReactDOM.render(elem, document.getElementById('root'))
+
+// function SayHello(props) {
+//     return <h1> Hello {props.name}</h1>
+// }
+// function App() {
+//     return (<div>
+//         <SayHello name="Ana" />
+//         <SayHello name="Vanja" />
+//         <SayHello name="Manja" />
+//     </div>)
+// }
+// ReactDOM.render(<App />, document.getElementById('root'))
