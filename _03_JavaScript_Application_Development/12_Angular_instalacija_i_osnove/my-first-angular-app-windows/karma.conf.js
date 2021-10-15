92c8e82ec36d05e89b238226e5250445955b453a
Dragan <milosevdragan1988@gmail.com>
2021-10-15 16:21:34 +0200

    fg
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- _03_JavaScript_Application_Development/12_Angular_instalacija_i_osnove/my-first-angular-app-windows/karma.conf.js
+++ _03_JavaScript_Application_Development/12_Angular_instalacija_i_osnove/my-first-angular-app-windows/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-first-app-angular'),
+      dir: require('path').join(__dirname, './coverage/my-first-angular-app-windows'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- _03_JavaScript_Application_Development/12_Angular_instalacija_i_osnove/my-first-angular-app-windows/src/app/app.component.spec.ts
+++ _03_JavaScript_Application_Development/12_Angular_instalacija_i_osnove/my-first-angular-app-windows/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-app-angular'`, () => {
+  it(`should have as title 'my-first-angular-app-windows'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app-angular');
+    expect(app.title).toEqual('my-first-angular-app-windows');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first-app-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first-angular-app-windows app is running!');
   });
 });
