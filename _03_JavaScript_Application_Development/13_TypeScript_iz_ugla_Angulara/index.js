545c60cc43f2ec08e3d40a1135108b3a46bc1b5b
Dragan <milosevdragan1988@gmail.com>
2021-10-16 20:37:03 +0200

    j
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

---
+++ _03_JavaScript_Application_Development/13_TypeScript_iz_ugla_Angulara/index.js
@@ -0,0 +1,46 @@
+function great(greeting, name) {
+    return greeting + ' ' + name + '!';
+}
+// function square(a: (number | string)): number {
+//     return <number>a * <number>a;
+// }
+// enum Day { Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7 };
+// // let day: Day = Day.Monday;
+// let day: number;
+// switch (day) {
+//     case Day.Monday:
+//         console.log('Today is Monday');
+//         break;
+//     case Day.Tuesday:
+//         console.log('Tuesday');
+//         break;
+//     case Day.Wednesday:
+//         console.log('Wednesday');
+//         break;
+// }
+// let myTuple: [string, boolean];
+// myTuple = ['sdasada', false];
+// let myArray: number[] = [8, 6, 2];
+// let myArray2: Array<number> = [8, 6, 2];
+// function infiniteLoop(): never {
+//     while (true) {
+//     }
+// }
+// function sayHello(message:string): never {
+//     throw new Error(message);
+// }
+// let notSure: any = 6;
+// function sayHeloo(name:string): void {
+//     console.log('Hello ' + name);
+//     // return "asdasdas"
+// }
+// let isDone: boolean = false;
+// let width: number = 33;
+// let message: string = "Hello world";
+// let myVar: null = null;
+// let myVar2: undefined = undefined;
+// let message : string = "Hello!";
+// function add (a:number, b:number) {
+//     return a + b;
+// }
+// add(2, 6)
