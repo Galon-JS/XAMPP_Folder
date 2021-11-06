6359882c8b4fc83687ab8a6a15ecd2616e86b7c8
Dragan <milosevdragan1988@gmail.com>
2021-11-06 01:42:04 +0100

    k
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- _03_JavaScript_Application_Development/14_Angular_moduli_i_komponente/angular-app/src/app/demo/demo.component.ts
+++ _03_JavaScript_Application_Development/14_Angular_moduli_i_komponente/angular-app/src/app/demo/demo.component.ts
@@ -3,19 +3,54 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css']
+  styleUrls: ['./demo.component.css'],
 })
 export class DemoComponent implements OnInit {
 
-  name: string;
-
+  sayHello() {
+    alert('Heloo');
+  }
+  // message: string;
+  
   constructor() {
-
-    this.name = "Ana";
-
-   }
-
-  ngOnInit(): void {
+    // this.message = 'This is initial value';
   }
 
+  ngOnInit(): void {}
 }
+// export class DemoComponent implements OnInit {
+//   name: string;
+//   name2: string;
+//   user: any;
+
+//   userClasses: any;
+//   userClasses2: any;
+
+//   width: string;
+//   width2: number;
+//   userStyles: string;
+
+//   constructor() {
+//     this.name = 'Ana';
+//     this.name2 = 'Tanja';
+//     this.user = {
+//       id: 1,
+//       name: 'Vanja',
+//       email: 'vanja@gmail.com',
+//       balance: 124367.675474,
+//       code: 'a4fg56',
+//       isDeleted: true
+//     }
+//     this.userClasses = ['user-class-1','user-class-2','user-class-3']
+//     this.userClasses2 = {
+//       'user-class-1': true,
+//       'user-class-2': false,
+//       'user-class-3': true
+//     }
+//     this.width = '100px'
+//     this.width2 = 100
+//     this.userStyles = 'width: 200px; height: 100px'
+//   }
+
+//   ngOnInit(): void {}
+// }
