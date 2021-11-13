89090ec7c80b65a3f9315b3738559a1319e49438
Dragan <milosevdragan1988@gmail.com>
2021-11-13 10:30:41 +0100

    s
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- _Vue.js_JAD1/js/events.js
+++ _Vue.js_JAD1/js/events.js
@@ -1,25 +1,25 @@
 const app = new Vue({
-    el: '#app',
-    data: {
-        count: 0,
-        bgColor: 'color'
+  el: '#app',
+  data: {
+    count: 0,
+    bgColor: 'color',
+  },
+  methods: {
+    alert_name: function() {
+      const inputYourName = document.querySelector('#your_name');
+      alert('You entered: ' + inputYourName.value);
     },
-    methods: {
-        alert_name: function () {
-            const inputYourName = document.querySelector('#your_name')
-            alert('You entered: ' + inputYourName.value)
-        },
-        increase_count: function () {
-            this.count++
-        },
-        decrease_count: function () {
-            this.count--
-        },
-        reset: function () {
-            this.count = 0
-        },
-        change_color: function (color) {
-            this.bgColor = color
-        }
-    }
-})
+    increase_count: function() {
+      this.count++;
+    },
+    decrease_count: function() {
+      this.count--;
+    },
+    reset: function() {
+      this.count = 0;
+    },
+    change_color: function(color) {
+      this.bgColor = color;
+    },
+  },
+});
