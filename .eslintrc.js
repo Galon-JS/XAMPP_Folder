be41bc7edc570f204f4b0aa9e10cb29d5318ed79
Dragan <milosevdragan1988@gmail.com>
2021-11-16 23:13:17 +0100

    d
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "es6": true
-    },
+    "env": [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:vue/essential',
+      '@vue/prettier'
+    ],
+
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended"
     ],
+
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
@@ -15,12 +18,39 @@ module.exports = {
         "ecmaVersion": 6,
         "sourceType": "module"
     },
+
     "plugins": [
         "react"
     ],
+
     "rules": {
         "semi": 0,
         "no-unused-vars": 0,
         "no-undef": 0
+    },
+
+    root: true,
+
+    env: {
+      browser: true,
+      es2021: true,
+      es6: true,
+      node: true
+    },
+
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    },
+
+    rules: {
+      semi: 0,
+      'no-unused-vars': 0,
+      'no-undef': 0,
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
 };
