1ef208bcbbeb3d1b24d94825f5986fac883ea0cc
Dragan <milosevdragan1988@gmail.com>
2021-11-17 06:56:37 +0100

    dfs
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,22 @@
 module.exports = {
-    "env": [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:vue/essential',
-      '@vue/prettier'
-    ],
-
-    "extends": [
+        "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:vue/recommended",
+        "plugin:angular/recommended"
     ],
-
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 6,
-        "sourceType": "module"
-    },
-
     "plugins": [
-        "react"
+      "react",
+      "vue",
+      "angular"
     ],
-
-    "rules": {
-        "semi": 0,
-        "no-unused-vars": 0,
-        "no-undef": 0
-    },
-
     root: true,
-
     env: {
       browser: true,
       es2021: true,
       es6: true,
       node: true
     },
-
     parserOptions: {
       ecmaFeatures: {
         jsx: true
@@ -45,12 +24,12 @@ module.exports = {
       ecmaVersion: 2020,
       sourceType: 'module'
     },
-
     rules: {
       semi: 0,
       'no-unused-vars': 0,
       'no-undef': 0,
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-constant-condition': 0
     }
 };
