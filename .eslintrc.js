f47d05a626050ad8bb40f9d895d4c2453d34f87f
Dragan <milosevdragan1988@gmail.com>
2021-12-05 21:38:15 +0100

    s
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         "extends": [
           'eslint:recommended',
           'plugin:react/recommended',
@@ -15,6 +16,8 @@ module.exports = {
       "vue3",
       "angular"
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
     "root": true,
@@ -38,6 +41,9 @@ module.exports = {
     "plugins": [
         "vue"
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
