6958a21f9dba4b404deb5c9966a221dbd5cc028b
Dragan <milosevdragan1988@gmail.com>
2021-12-05 21:44:03 +0100

    sef
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         "extends": [
           'eslint:recommended',
           'plugin:react/recommended',
@@ -18,6 +19,8 @@ module.exports = {
       "vue3",
       "angular"
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -50,6 +53,9 @@ module.exports = {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
